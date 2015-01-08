5a00f7e333ba4a526983774a8eeb43caee766b59
Dan Fox <iamdanfox@gmail.com>
2015-01-08 18:07:27 +0000

    Cargo bench now reports 0.32! success :)

--- src/main.rs
+++ src/main.rs
@@ -12,28 +12,9 @@ mod bench;
 
 
 fn main() {
-
   let mut game_state = GameState::new(2);
-  let mut i = 0u;
-
-
   loop {
-    // println!("\n\n{}\n", game_state.board);
-    // println!("{}: player {} turn (score = {})", i, game_state.turn, game_state.players[game_state.turn].score);
-
-    // let moves:Vec<(Score, Move)> = game_state.generate_moves();
-
-    // let mut best = None;
-    // for pair in moves.iter() {
-    //   best = match best {
-    //     None => Some(pair),
-    //     Some(ref m) if pair.0 > (*m).0 => Some(pair),
-    //     _ => best
-    //   }
-    // };
-
     let best = game_state.generate_best_move();
-
     match best {
       None => break,
       Some((score, chosen_move)) => {
